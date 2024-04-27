debad0c65a4457c04f9c4275e708af97059958d8
tom-whitehead <99190557+tom-whitehead@users.noreply.github.com>
2024-04-27 12:26:50 +0100

    add chebyshev distance (#12)

--- src/distance.rs
+++ src/distance.rs
@@ -6,6 +6,7 @@ use num_traits::Float;
 pub enum DistanceMetric {
     Euclidean,
     Manhattan,
+    Chebyshev,
 }
 
 impl DistanceMetric {
@@ -13,6 +14,7 @@ impl DistanceMetric {
         match *self {
             Self::Euclidean => { euclidean_distance(a, b) }
             Self::Manhattan => { manhattan_distance(a, b) }
+            Self::Chebyshev => { chebyshev_distance(a, b) }
         }
     }
 }
@@ -21,10 +23,12 @@ pub(crate) fn get_dist_func<'a, T: Float>(metric: &DistanceMetric) -> impl Fn(&[
     match metric {
         DistanceMetric::Euclidean => { euclidean_distance }
         DistanceMetric::Manhattan => { manhattan_distance }
+        DistanceMetric::Chebyshev => { chebyshev_distance }
     }
 }
 
 pub(crate) fn euclidean_distance<T: Float>(a: &[T], b: &[T]) -> T {
+    assert_inputs(&a, &b);
     a.iter()
         .zip(b.iter())
         .map(|(x, y)| ((*x) - (*y)) * ((*x) - (*y)))
@@ -33,8 +37,23 @@ pub(crate) fn euclidean_distance<T: Float>(a: &[T], b: &[T]) -> T {
 }
 
 pub(crate) fn manhattan_distance<T: Float>(a: &[T], b: &[T]) -> T {
+    assert_inputs(&a, &b);
     a.iter()
         .zip(b.iter())
         .map(|(x, y)| ((*x) - (*y)).abs())
         .fold(T::zero(), std::ops::Add::add)
+}
+
+pub(crate) fn chebyshev_distance<T: Float>(a: &[T], b: &[T]) -> T {
+    assert_inputs(&a, &b);
+    a.iter()
+        .zip(b.iter())
+        .map(|(x, y)| ((*x) - (*y)).abs())
+        .max_by(|a, b| a.partial_cmp(b).expect("inputs were validated"))
+        .expect("inputs were validated")
+}
+
+fn assert_inputs<T: Float>(a: &[T], b: &[T]) {
+    assert_eq!(a.len(), b.len());
+    assert!(!a.is_empty());
 }
