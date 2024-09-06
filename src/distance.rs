21af3ee97fed3e981ac3e846b8dea63405fbe655
Rahul Garg <rg@vihu.dev>
2024-09-05 20:31:08 -0500

    Add support for haversine_distance

--- src/distance.rs
+++ src/distance.rs
@@ -7,12 +7,14 @@ pub enum DistanceMetric {
     Euclidean,
     Manhattan,
     Chebyshev,
+    Haversine,
 }
 
 impl DistanceMetric {
     pub(crate) fn calc_dist<T: Float>(&self, a: &[T], b: &[T]) -> T {
         match *self {
             Self::Euclidean => euclidean_distance(a, b),
+            Self::Haversine => haversine_distance(a, b),
             Self::Manhattan => manhattan_distance(a, b),
             Self::Chebyshev => chebyshev_distance(a, b),
         }
@@ -24,7 +26,31 @@ pub(crate) fn get_dist_func<T: Float>(metric: &DistanceMetric) -> impl Fn(&[T], 
         DistanceMetric::Euclidean => euclidean_distance,
         DistanceMetric::Manhattan => manhattan_distance,
         DistanceMetric::Chebyshev => chebyshev_distance,
+        DistanceMetric::Haversine => haversine_distance,
     }
+}
+
+pub(crate) fn haversine_distance<T: Float>(a: &[T], b: &[T]) -> T {
+    assert_inputs(a, b);
+    assert_eq!(a.len(), 2, "Need both lat and lon");
+
+    let (lat1, lon1) = (a[0], a[1]);
+    let (lat2, lon2) = (b[0], b[1]);
+
+    // Earth's radius in meters
+    let r = T::from(6371000.0).unwrap();
+
+    let dlat = (lat2 - lat1).to_radians();
+    let dlon = (lon2 - lon1).to_radians();
+
+    let lat1 = lat1.to_radians();
+    let lat2 = lat2.to_radians();
+
+    let a = (dlat / T::from(2.0).unwrap()).sin().powi(2)
+        + lat1.cos() * lat2.cos() * (dlon / T::from(2.0).unwrap()).sin().powi(2);
+    let c = T::from(2.0).unwrap() * a.sqrt().asin();
+
+    r * c
 }
 
 pub(crate) fn euclidean_distance<T: Float>(a: &[T], b: &[T]) -> T {
